70c1cc779b6ed392c6f94f03261cf2ff619a638a
IvanZudilov <ivanzudilov9@gmail.com>
2019-01-04 21:31:04 +0300

    initial commit

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1546625890,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/stopblock-sheet0.png",
+		"images/dirth-sheet0.png",
+		"images/tree-sheet0.png",
+		"images/money-sheet0.png",
+		"images/healthspr-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
